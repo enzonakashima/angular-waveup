8f1afa36b5a9048638fa480dcd9203a79264681f
enzonakashima <enzonakashima@outlook.com>
2021-05-26 08:12:31 -0300

    Criação do Login Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([{ path: '', component: LoginComponent }])
+  ],
+  declarations: [AppComponent, HelloComponent, LoginComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
