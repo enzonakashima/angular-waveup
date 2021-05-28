bd298bba16092842873116d8b08f7e84eb8d2d8c
enzonakashima <enzonakashima@outlook.com>
2021-05-27 21:24:43 -0300

    Usuário, data e hora do evento

--- src/app/events.ts
+++ src/app/events.ts
@@ -1,6 +1,8 @@
 export const events = [
   {
     id: 1,
+    user: 'Prefeitura de Guarujá',
+    date: 'ontem às 15:40',
     name: 'Campeonato de Surf Baixada Santista',
     price: 40,
     description:
@@ -8,6 +10,8 @@ export const events = [
   },
   {
     id: 2,
+    user: 'Confederação Brasileira de Canoagem',
+    date: 'quarta-feira às 12:30',
     name: 'Campeonato Brasileiro de Canoagem',
     price: 50,
     description:
@@ -15,12 +19,16 @@ export const events = [
   },
   {
     id: 3,
+    user: 'Amyr Klink',
+    date: 'terça-feira às 09:21',
     name: 'Palestra do Amyr Klink',
     price: 30,
     description: 'Palestra com o navegador Amyr Klink | Gonzaga, Santos'
   },
   {
     id: 4,
+    user: 'Supsix',
+    date: 'segunda-feira às 21:09',
     name: 'Aulas de Stand Up',
     price: 0,
     description: 'Aulas grátis de Stand Up | Ponta da Praia, Santos '
